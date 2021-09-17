f4f3e3dd028b6ae9780eb56612687626e1898333
Illia Iliashenko <marple228@hotmail.com>
2021-09-17 17:09:34 +0300

    Added ApartmentPhotoService

---
+++ Booking/Booking.BLL/Services/Booking/ApartmentPhotoService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Booking.BLL.Services.Booking.Interfaces;
+using Booking.DAL.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Booking.BLL.Services.Booking
+{
+    public class ApartmentPhotoService : IApartmentPhotoService
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private readonly IApartmentPhotoRepository _apartmentPhotoRepository;
+
+        public ApartmentPhotoService(
+            IWebHostEnvironment hostingEnvironment, 
+            IApartmentPhotoRepository apartmentPhotoRepository)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _apartmentPhotoRepository = apartmentPhotoRepository;
+        }
+
+        public async Task<IEnumerable<string>> GetApartmentPhotoPath(int id)
+        {
+            var photoPathOriginal = await _apartmentPhotoRepository.GetApartmentPhotoPath(id);
+
+            var photoPathRelative = photoPathOriginal.Select(p => Path.Combine(_hostingEnvironment.WebRootPath, p));
+
+            return photoPathRelative;
+        }
+    }
+}
---
+++ Booking/Booking.BLL/Services/Booking/Interfaces/IApartmentPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Booking.BLL.Services.Booking.Interfaces
+{
+    public interface IApartmentPhotoService
+    {
+        Task<IEnumerable<string>> GetApartmentPhotoPath(int id);
+    }
+}

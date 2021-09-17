85a3b1fc17063b653e9bd0704cb17c811bb68b08
Illia Iliashenko <marple228@hotmail.com>
2021-09-17 14:32:01 +0300

    Added ApartmentRepository

---
+++ Booking/Booking.DAL/Data/Repositories/ApartmentRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Booking.DAL.Data.Repositories.Interfaces;
+using Booking.DAL.Models.Booking;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.DAL.Data.Repositories
+{
+    public class ApartmentRepository : IApartmentRepository
+    {
+        private readonly BookingContext _bookingContext;
+
+        public ApartmentRepository(BookingContext bookingContext)
+        {
+            _bookingContext = bookingContext;
+        }
+
+        public async Task<IEnumerable<ApartmentEntity>> GetAllApartmentsAsync()
+        {
+            var apartments = await _bookingContext
+                .Apartments
+                .Include(a => a.Details)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return apartments;
+        }
+
+        public async Task<IEnumerable<ApartmentEntity>> GetApartmentByNameAsync(string name)
+        {
+            var apartments = await _bookingContext
+                .Apartments
+                .Where(ap=>ap.Name.StartsWith(name))
+                .Include(a => a.Details)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return apartments;
+        }
+    }
+}

a59ff1143e7cb473a46ab0c97b6717c79e8c80fe
Illia Iliashenko <marple228@hotmail.com>
2021-09-18 15:04:06 +0300

    changes in ApartmentViewModel

---
+++ Booking/Booking/Models/Booking/ApartmentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Booking.Models.Booking
+{
+    public class ApartmentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+
+        public IDictionary<string,string> Details { get; set; }
+        public IEnumerable<string> Photos { get; set; }
+    }
+}

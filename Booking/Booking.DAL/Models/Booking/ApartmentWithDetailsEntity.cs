4d03c1b4239abae6519b4c1bc70de1cacb6b4c2f
Illia Iliashenko <marple228@hotmail.com>
2021-09-18 15:01:57 +0300

    Added new DAL entities

---
+++ Booking/Booking.DAL/Models/Booking/ApartmentWithDetailsEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Booking.DAL.Models.Booking
+{
+    public class ApartmentWithDetailsEntity
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public IDictionary<string,string> Details { get; set; }
+    }
+}
---
+++ Booking/Booking.DAL/Models/Booking/SingleDetailEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Booking.DAL.Models.Booking
+{
+    public class SingleDetailEntity
+    {
+        public int ApartmentId { get; set; }
+        public string Value { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}

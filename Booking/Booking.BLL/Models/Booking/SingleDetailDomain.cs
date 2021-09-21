5ea6cc7781815be39f20f23258225e4f8438eea3
Illia Iliashenko <marple228@hotmail.com>
2021-09-21 20:12:13 +0300

    deleted unused data

--- Booking/Booking.BLL/Models/Booking/SingleDetailDomain.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Booking.BLL.Models.Booking
-{
-    public class SingleDetailDomain
-    {
-        public string Value { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
-    }
-}

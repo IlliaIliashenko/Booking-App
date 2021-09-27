5d5c0d422ff9e64c96df066ed7714a9c4a87fbd7
Illia Iliashenko <marple228@hotmail.com>
2021-09-27 18:56:12 +0300

    added userService implementation

---
+++ Booking/Booking.BLL/Services/Authentication/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Booking.BLL.Services.Authentication.Interfaces;
+using Booking.Common.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Booking.BLL.Services.Authentication
+{
+    public class UserService: IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public OperationResult<string> GetCurrentUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            return OperationResult<string>.FromResult(userId);
+        }
+    }
+}

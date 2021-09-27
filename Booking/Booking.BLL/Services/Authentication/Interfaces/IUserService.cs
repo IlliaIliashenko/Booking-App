5daccdc6efbcdb4558e258f686a99ff97d761638
Illia Iliashenko <marple228@hotmail.com>
2021-09-27 18:55:37 +0300

    added userService

---
+++ Booking/Booking.BLL/Services/Authentication/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Booking.Common.Models;
+
+namespace Booking.BLL.Services.Authentication.Interfaces
+{
+    public interface IUserService
+    {
+        OperationResult<string> GetCurrentUserId();
+    }
+}
